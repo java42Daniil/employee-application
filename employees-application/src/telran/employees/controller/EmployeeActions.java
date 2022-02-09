5d2921ab66cac23be04de8b961c2de1aef7cef6e
Daniil Berezin <daniil.berezin98@gmail.com>
2022-02-09 14:21:42 +0200

    hw37-Berezin Daniil

---
+++ employees-application/src/telran/employees/controller/EmployeeActions.java
@@ -0,0 +1,94 @@
+package telran.employees.controller;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import telran.employees.dto.Employee;
+import telran.employees.services.EmployeesMethods;
+import telran.view.InputOutput;
+import telran.view.Item;
+
+public class EmployeeActions {
+	private static final int MIN_SALARY = 3000;
+	private static final int MAX_SALARY = 15000;
+	private static final int MIN_AGE = 18;
+	private static final int MAX_AGE = 120;
+	private static final HashSet<String> departments = new HashSet<>(Arrays.asList("qa", "developer"));
+	private static EmployeesMethods employees;
+	private EmployeeActions(){
+	}
+	static public ArrayList<Item> getEmployeesMenuItems(EmployeesMethods employees){
+		EmployeeActions.employees = employees;
+		ArrayList<Item> items = new ArrayList<>();
+		items.add(Item.of(" Add new Employee", EmployeeActions::addEmployee));
+		items.add(Item.of(" Remove Employee", EmployeeActions::removeEmployee));
+		items.add(Item.of(" Get Employee by ID", EmployeeActions::getEmployee));
+		items.add(Item.of(" Get all Employees", EmployeeActions::getAllEmployees));
+		items.add(Item.of(" Get Employees by age", EmployeeActions::getEmployeesByAge));
+		items.add(Item.of(" Get Employees by salary", EmployeeActions::getEmployeesBySalary));
+		items.add(Item.of(" Get Employees by department", EmployeeActions::getEmployeesByDepartment));
+		items.add(Item.of(" Get Employees by depatment and salary", EmployeeActions::getEmployeesByDepartmentAndSalary));
+		items.add(Item.of(" Update salary", EmployeeActions::updateSalary));
+		items.add(Item.of("Update department", EmployeeActions::updateDepartment));
+		items.add(Item.exit());
+		return items;
+	}
+	static private Employee enterEmployee(InputOutput io) {
+		long id = EmployeeActions.getEmployeeId(io);
+		String name = io.readStringPredicate("Enter name", "Name may contain only letters with first capital",
+				str -> str.matches("[A-Z][a-z]+"));
+		LocalDate birthDate = io.readDate("Enter birthdate in the yyyy-MM-dd format");
+		int salary = io.readInt("Enter Salary", 0, Integer.MAX_VALUE-1);
+		String department = io.readStringOption("Enter department from list" + departments, departments).toLowerCase();
+		return  new Employee(id, name, birthDate, salary, department);
+    }
+	private static long getEmployeeId(InputOutput io) {
+		return Long.parseLong(io.readStringPredicate("enter id", "wrong format for ID", str -> str.matches("\\d{9}")));
+	}
+	static private void addEmployee(InputOutput io) {
+		io.writeObjectLine(employees.addEmployee(EmployeeActions.enterEmployee(io)));
+	}
+	static private  void removeEmployee(InputOutput io) {
+		io.writeObjectLine(employees.removeEmployee(EmployeeActions.getEmployeeId(io)));
+	}
+	static private  void getAllEmployees(InputOutput io) {
+		 io.writeObjectLine(employees.getAllEmployees());
+	}
+	static private  void getEmployee(InputOutput io) {
+		io.writeObjectLine(employees.getEmployee(EmployeeActions.getEmployeeId(io)));
+	}
+	static private  void getEmployeesByAge(InputOutput io) {
+		int ageFrom = io.readInt("enter age range FROM", MIN_AGE, MAX_AGE-1);
+		int ageTo = io.readInt("enter age range TO",ageFrom+1, MAX_AGE);
+		io.writeObjectLine((employees.getEmployeesByAge(ageFrom, ageTo)));
+	}
+	static private  void updateSalary(InputOutput io) {
+		long emplID = EmployeeActions.getEmployeeId(io);
+		int actualSalary = employees.getEmployee(emplID).salary;
+		int newSalary = io.readInt("Enter new Salary", actualSalary+1, Integer.MAX_VALUE);
+		io.writeObjectLine(employees.updateSalary(emplID, newSalary));
+	}
+	static private  void getEmployeesBySalary(InputOutput io) {
+		int minSalary = io.readInt("Enter min range for salary", MIN_SALARY, Integer.MAX_VALUE-1);
+		int maxSalary = io.readInt("Enter max range for salary", minSalary+1, Integer.MAX_VALUE);
+		io.writeObjectLine(employees.getEmployeesBySalary(minSalary, maxSalary));
+	}
+	static private  void updateDepartment(InputOutput io) {
+		long emplID = EmployeeActions.getEmployeeId(io);
+		io.writeObjectLine("Now works in " + employees.getEmployee(emplID).department);
+		String newDepartment = io.readStringOption("enter new department from list" + departments, departments).toLowerCase();
+		io.writeObjectLine(employees.updateDepartment(emplID, newDepartment));
+	}
+	static private  void getEmployeesByDepartment(InputOutput io) {
+		String department = io.readStringOption("Enter department " + departments, departments).toLowerCase();
+		io.writeObjectLine(employees.getEmployeesByDepartment(department));
+	}
+	static private  void getEmployeesByDepartmentAndSalary(InputOutput io) {
+		String department = io.readStringOption("Enter department from list" + departments, departments).toLowerCase();
+		int minSalary = io.readInt("Enter min range for salary",MIN_SALARY, Integer.MAX_VALUE-1);
+		int maxSalary = io.readInt("Enter max range for salary", minSalary+1, Integer.MAX_VALUE);
+		io.writeObjectLine(employees.getEmployeesByDepartmentAndSalary(department, minSalary, maxSalary));
+	}
+}
--- employees-application/src/telran/view/InputOutput.java
+++ employees-application/src/telran/view/InputOutput.java
@@ -1,14 +1,14 @@
-
 package telran.view;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 public interface InputOutput {
+	String NO_NUMBER = "No number";
+	String NO_OPTION = "Wrong input (see possible options)";
 	String readString(String prompt);
 	void writeObject(Object obj);
 	default void writeObjectLine(Object obj) {
@@ -36,59 +36,35 @@ public interface InputOutput {
 				return str;
 			}
 			throw new IllegalArgumentException();
-		});
+		}); 
 	}
-	//TODO write all default methods from UML schema 
-	default Integer readInt(String prompt) {
-		String errorMessage = "This is not number";
-		return readObject(prompt, errorMessage, n -> {
-			try {
-				return Integer.parseInt(n);
-			} catch (NumberFormatException e) {
-				throw new IllegalArgumentException();
-			}
-		});
+	default Integer readInt( String prompt) {
+		return readObject(prompt, NO_NUMBER, Integer::parseInt);
+	}
+	default Double readDouble(String prompt) {
+		return readObject(prompt, NO_NUMBER, Double::parseDouble);
 	}
 	default Integer readInt(String prompt, int min, int max) {
-		String errorMessage = String.format("Input must be integer in range[%d,%d]", min, max);
-		try {
-			return readObject(prompt, errorMessage, str -> {
-				int value = Integer.parseInt(str);
-				if(value<min || value>max) {
-					throw new IllegalArgumentException();
-				}
-				return value;
-			});
-		} catch(NumberFormatException ex) {
-			throw new IllegalArgumentException();
-		}
-	}
-	default Long readLong(String prompt) {
-		String errorMessage = "Input must be number";
-		return readObject(prompt, errorMessage, n -> {
-			try {
-				return Long.parseLong(n);
-			} catch (NumberFormatException e) {
+		return readObject(prompt, String.format("No number in the range [%d-%d]",min,max), str -> {
+			int res = Integer.parseInt(str);
+			if (res < min || res > max) {
 				throw new IllegalArgumentException();
 			}
+			return res;
 		});
+	}
+	default Long readLong( String prompt) {
+		return readObject(prompt, NO_NUMBER, Long::parseLong);
+		
 	}
 	default String readStringOption(String prompt, Set<String> options) {
-		return readStringPredicate(prompt, "Out of options", str -> {
-			return options.contains(str);
-		});
+		return readStringPredicate(prompt, NO_OPTION, options::contains);
+		
 	}
-	default LocalDate readDate(String prompt) {
-		DateTimeFormatter dtf = DateTimeFormatter.ISO_DATE;
-		return readDate(prompt + " in format yyyy-mm-dd" , dtf);
+	default LocalDate readDate(String prompt)  {
+		return readObject(prompt, "Wrong date in yyyy-MM-dd format", LocalDate::parse);
 	}
-	default LocalDate readDate(String prompt, DateTimeFormatter formatter) {
-		try {
-			return readObject(prompt, "incorrect data format", str -> {
-				return LocalDate.parse(str, formatter);
-			});
-		} catch (DateTimeParseException ex) {
-			throw new IllegalArgumentException();
-		}
+	default LocalDate readDate(String prompt,  DateTimeFormatter formatter) {
+		return readObject(prompt, "Wrong date in the specified format", str -> LocalDate.parse(str, formatter));
 	}
 }
